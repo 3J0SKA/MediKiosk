24a0f03a02e51bb187f395b35e24756810b0a385
rudranshuojha <rudranshuojha601@gmail.com>
2026-09-07 18:14:28 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -32,7 +32,10 @@ from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 
-CORS(app)
+CORS(app, origins=[
+   # "http://localhost:3000",                   # Local development frontend
+    "testing1-meow-dd1f.vercel.app"  # production Vercel URL
+])
 
 @app.route("/")
 def index():
